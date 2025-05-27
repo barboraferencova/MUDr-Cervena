47ab437006159227588bb53f9532f52c7d52ea93
barboraferencova <ferencovab@seznam.cz>
2025-05-27 16:54:20 +0200

    Plně funkční navbar v desktopu a na mobilu

--- script.js
+++ script.js
@@ -1,31 +1,41 @@
 
-// Navbar funkce pro otevírání a zavíraní mobilního menu + funkce pro uzavření skrze overlay
 const navbar = document.getElementById("navbar");
-const openSideBar = document.getElementById("open-sidebar-button");
+const openSidebarBtn = document.getElementById("open-sidebar-button");
+const closeSidebarBtn = document.getElementById("close-sidebar-button");
+const media = window.matchMedia("(min-width: 1150px)");
 
-const media = window.matchMedia("(width < 700px)");
-media.addEventListener("change", (e) => updateNavbar(e));
+media.addEventListener("change", updateNavbar);
 
 function updateNavbar(e) {
-const isMobile = e.matches;
-if(isMobile) {
-    navbar.setAttribute("inert", "");
-} else {
+  const isDesktop = e.matches;
+
+  if (isDesktop) {
     navbar.removeAttribute("inert");
-}
-
-}
-
-function openSidebar(){
-    navbar.classList.add("show");
-    openSideBar.setAttribute("aria-expanded", "true");
-    navbar.removeAttribute("inert");
-}
-
-function closeSidebar(){
     navbar.classList.remove("show");
-    openSideBar.setAttribute("aria-expanded", "false");
+    openSidebarBtn.style.display = "none";
+    closeSidebarBtn.style.display = "none";
+  } else {
     navbar.setAttribute("inert", "");
+    openSidebarBtn.style.display = "block";
+    closeSidebarBtn.style.display = "none";
+  }
 }
 
-updateNavbar(media)
+function openSidebar() {
+  navbar.classList.add("show");
+  openSidebarBtn.setAttribute("aria-expanded", "true");
+  openSidebarBtn.style.display = "none";
+  navbar.removeAttribute("inert");
+  closeSidebarBtn.style.display = "block";
+}
+
+function closeSidebar() {
+  navbar.classList.remove("show");
+  openSidebarBtn.setAttribute("aria-expanded", "false");
+  openSidebarBtn.style.display = "block";
+  openSidebarBtn.removeAttribute("aria-hidden");  
+  navbar.setAttribute("inert", "");
+  closeSidebarBtn.style.display = "none"; 
+}
+
+updateNavbar(media);
